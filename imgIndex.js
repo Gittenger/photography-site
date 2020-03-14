504a1ecf14a2393f624ba97f53dc1842fdebbd4d
Gittenger <john.pittenger86@gmail.com>
2020-03-14 14:30:10 -0400

    initial, basic func, needs cleaned up

---
+++ imgIndex.js
@@ -0,0 +1,170 @@
+const photos = [
+  {
+    id: "1",
+    url: "img/img-1.jpg",
+    photographer: "Elia Pellegrini",
+    handle: "@eliapelle",
+    location: { coords: [], text: "" },
+    other: {
+      text: "Astronaut 07 @eliapelle https://eliapellle.bigcartel.com/products"
+    }
+  },
+  {
+    id: "2",
+    url: "/img/img-2",
+    photographer: "Houcine Ncib",
+    handle: "@houcinencibphotography",
+    location: { coords: [], text: "" },
+    other: { text: "Male street Fashion Editorial 2019." }
+  },
+  {
+    id: "3",
+    url: "/img/img-3",
+    photographer: "Eugene Chystiakov",
+    handle: "@eugenechystiakov",
+    location: {
+      coords: [],
+      text: "Київ, Київ, Україна"
+    },
+    other: { text: "" }
+  },
+  {
+    id: "4",
+    url: "/img/img-4",
+    photographer: "Spencer Davis",
+    handle: "@spencerdavis",
+    location: { coords: [], text: "Nile River, Cairo, Egypt" },
+    other: { text: "" }
+  },
+  {
+    id: "5",
+    url: "/img/img-5",
+    photographer: "Agnieszka Kowalczyk",
+    handle: "@koalamoose",
+    location: { coords: [], text: "Kudawa, Sri Lanka" },
+    other: { text: "" }
+  },
+  {
+    id: "6",
+    url: "/img/img-6",
+    photographer: "Olena Sergienko",
+    handle: "@olenkasergienko",
+    location: { coords: [], text: "Kyiv, Україна" },
+    other: { text: "" }
+  },
+  {
+    id: "7",
+    url: "/img/img-7",
+    photographer: "Alex Azabache",
+    handle: "@alexazabache",
+    location: { coords: [], text: "Ubud, Gianyar, Bali, Indonesia" },
+    other: { text: "Walking towards the waterfall in Ubud, Bali" }
+  },
+  {
+    id: "8",
+    url: "/img/img-8",
+    photographer: "Zoltan Tasi",
+    handle: "@zoltantasi",
+    location: {
+      coords: [],
+      text: "London Eye Pier, London, Egyesült Királyság"
+    },
+    other: { text: "London Eye" }
+  },
+  {
+    id: "9",
+    url: "/img/img-9",
+    photographer: "Clay Banks",
+    handle: "@claybanks",
+    location: { coords: [], text: "Antelope Canyon, Arizona, USA" },
+    other: { text: "Exploring the Arizona Sandstone Part 2 (IG: @clay.banks)" }
+  },
+  {
+    id: "10",
+    url: "/img/img-10",
+    photographer: "Sabina Tone",
+    handle: "@etonesabina",
+    location: { coords: [], text: "Almaty, Kazakhstan" },
+    other: { text: "Tenderness" }
+  },
+  {
+    id: "11",
+    url: "/img/img-11",
+    photographer: "Sabina Tone",
+    handle: "@etonesabina",
+    location: { coords: [], text: "Shymbulak, Almaty, Kazakhstan" },
+    other: { text: "Freedom" }
+  },
+  {
+    id: "12",
+    url: "/img/img-12",
+    photographer: "Lesia and Serhii Artymovych",
+    handle: "@lesiako",
+    location: { coords: [], text: "" },
+    other: { text: "" }
+  },
+  {
+    id: "13",
+    url: "/img/img-13",
+    photographer: "Yiran Ding",
+    handle: "@yiranding",
+    location: { coords: [], text: "" },
+    other: { text: "" }
+  },
+  {
+    id: "14",
+    url: "/img/img-14",
+    photographer: "Jon Tyson",
+    handle: "@jontyson",
+    location: { coords: [], text: "" },
+    other: { text: "" }
+  },
+  {
+    id: "15",
+    url: "/img/img-15",
+    photographer: "Jon Tyson",
+    handle: "@jontyson",
+    location: { coords: [], text: "" },
+    other: { text: "" }
+  },
+  {
+    id: "16",
+    url: "/img/img-16",
+    photographer: "Adrian Infernus",
+    handle: "@adrian_infernus",
+    location: { coords: [], text: "" },
+    other: { text: "" }
+  },
+  {
+    id: "17",
+    url: "/img/img-17",
+    photographer: "Wilfried Santer",
+    handle: "@wsanter",
+    location: { coords: [], text: "" },
+    other: { text: "Lake stage at Bregenz, Austria. Opera Rigoletto" }
+  },
+  {
+    id: "18",
+    url: "/img/img-18",
+    photographer: "Christian Fregnan",
+    handle: "@christianfregnan",
+    location: { coords: [], text: "Museum Voorlinden, Wassenaar, Netherlands" },
+    other: { text: "swimming pool" }
+  },
+  {
+    id: "19",
+    url: "/img/img-19",
+    photographer: "Matthew LeJune",
+    handle: "@matthewlejune",
+    location: { coords: [], text: "New Orleans, United States" },
+    other: { text: "" }
+  },
+  {
+    id: "20",
+    url: "/img/img-20",
+    photographer: "Matthew LeJune",
+    handle: "@matthewlejune",
+    location: { coords: [], text: "New Orleans, United States" },
+    other: { text: "" }
+  }
+];
