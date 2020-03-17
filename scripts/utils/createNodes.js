5924bbb10b033a9b0ec4a8ea9e3aaad40ced603e
Gittenger <john.pittenger86@gmail.com>
2020-03-17 18:09:23 -0400

    fleshed out dom with figs/figcaption

--- scripts/utils/createNodes.js
+++ scripts/utils/createNodes.js
@@ -2,10 +2,34 @@ import photos from "../ref/imgIndex.js";
 
 export default function createNodes(grid) {
   for (let i = 1; i <= 40; i++) {
-    const node = document.createElement("img");
-    node.setAttribute("src", photos[i - 1].url);
-    node.classList.add("grid-item");
-    node.classList.add(`grid-item--${i}`);
-    grid.appendChild(node);
+    //create outer box for images, apply classes
+    const imgBox = document.createElement("div");
+    const gridItemClasses = ["grid-item", `grid-item--${i}`, "image-box"];
+    imgBox.classList.add(...gridItemClasses);
+
+    //create figure, append to box
+    const fig = document.createElement("figure");
+    fig.classList.add("figure");
+    imgBox.appendChild(fig);
+
+    //create image wrapper, append to fig
+    const wrapper = document.createElement("div");
+    wrapper.classList.add("image-wrapper");
+    fig.appendChild(wrapper);
+    fig.appendChild(document.createElement("figcaption"));
+    const captionText = `Photographer: ${
+      photos[i - 1].photographer
+    }; Instagram: ${photos[i - 1].handle}`;
+    const caption = document.createTextNode(captionText);
+    fig.querySelector("figcaption").appendChild(caption);
+    //set tooltip to caption
+    imgBox.setAttribute("title", captionText);
+
+    const img = document.createElement("img");
+    img.setAttribute("src", photos[i - 1].url);
+
+    wrapper.appendChild(img);
+
+    grid.appendChild(imgBox);
   }
 }
